ff7811e24538508761e5329ad3eee9c4b00eb8ce
Gabriel <gabriel.jchen@gmail.com>
2021-03-08 19:42:00 +0000

    created test file

---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+assertEqual(words.length, 3);
