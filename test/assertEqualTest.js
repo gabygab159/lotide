6d89af258d9ca945d7d83d2578023f1c7274e775
Gabriel <gabriel.jchen@gmail.com>
2021-03-08 19:19:19 +0000

    created test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("this", "that");
+assertEqual("This", "This");
+assertEqual(9, 1);
