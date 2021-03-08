32998dca806c00c891b5cccc75994d75ee0a5f2c
Gabriel <gabriel.jchen@gmail.com>
2021-03-08 19:18:42 +0000

    exported function

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅: " + actual + " === " + expected);
+    console.log(`✅✅✅: ${actual} === ${expected}`);
   } else {
-    console.log("❌❌❌: " + actual + " !== " + expected);
+    console.log(`❌❌❌: ${actual} !== ${expected}`);
   }
-
-
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("this", "that");
-assertEqual("This", "This");
-assertEqual(9, 1);
+
+
+module.exports = assertEqual;
